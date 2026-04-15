51308dfc1da9bb22feb95ea6f2015bf6298e03ff
danah04 <danahaldosary1@gmail.com>
2026-04-15 11:31:53 +0300

    completed todo 3

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 
 // db schema
+const songSchema = new mongoose.Schema({
+        title:  { type: String, required: true, trim: true },
+        artist: { type: String, required: true, trim: true },
+        year:   { type: Number, min: 1000, max: 2023 }
+      }, { timestamps: true });
 
 export const Song = mongoose.model("Song", songSchema);
--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -4,26 +4,33 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 /*Steps:
- *    - Create the environment file with name ".env" inside the 7-2-restful-api\server folder
- *    - Create two variables:
- *        - MONGO_URL=
- *        - PORT=5174
- *    - Copy connection string from your mongo cloud account: clusters->connection->MongoDB for VS Code
- *    - Save the connection string to MONGO_URL variable.
- *    - Replace the <db_password> with your database passowrd.
- *    - At the end of the connection string after back slash "/", write swe363_songs.
- *        For Example: mongodb+srv://<db_username>:<db_password>@cluster0.oh7ap07.mongodb.net/swe363_songs
- *    - Write your db_username & db_password in the connection string.
- *    - If connection successful → console.log("Mongo connected").
- *    - If error → console.error("Connection error:", err.message)
+ =================================================================
+ *  TASK 2 — Create Schema & Model (file: server/models/song.model.js)
+ *  =================================================================
+ *  Goal:
+ *    - Define a Song schema with fields:
+ *        title (String, required)
+ *        artist (String, required)
+ *        year (Number)
+ *    - Export a Mongoose model named "Song".
  *
- *  Hint:
- *    await mongoose.connect(process.env.MONGO_URL); */
+ *  Example:
+ *    name: { type: String, required: true, trim: true }
+ *      Note: trim is used to remove the extra spaces automatically.
+ * 
+ *  Syntax hint:
+ * 
+      const songSchema = new mongoose.Schema({
+        title:  { type: _______, required: _____, trim: _____ },
+        artist: { type: _______, required: _____, trim: _____ },
+        year:   { type: _______, min: _____, max: _____ }
+      }, { timestamps: _____ });
+
+      const Song = mongoose.model("Song", songSchema);
+ */
 
 
 // import dotenv and load environment variables from .env
-await mongoose.connect(process.env.MONGO_URL);
-dotenv.config();    
 
 import { connectDB } from "./db.js";
 import { Song } from "./models/song.model.js";
@@ -35,6 +42,9 @@ app.use(cors());
 app.use(express.json());
 
 await connectDB(process.env.MONGO_URL);
+
+dotenv.config();
+await mongoose.connect(process.env.MONGO_URL);
 
 // api/songs (Read all songs)
 
